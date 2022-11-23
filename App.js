033b8e73602c812496a59a0c21ccf778eea54525
abhishekshandilyaofficial <abhishekshandilyaofficial@gmail.com>
2022-11-23 15:31:41 +0530

    added till use reducer

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Ball from './components/Ball'
+function App() {
+  return (
+    <>
+      <Ball></Ball>
+    </>
+  );
+}
+
+export default App;
---
+++ components/Ball.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState, useReducer } from 'react'
+
+function reducer(state, action){
+    switch (action.type){
+        case "increment":
+            return state+1;
+        case "decrement":
+            return state-1;
+        default:
+            console.log("Wrong type");
+    }
+}
+function Ball() {
+    // let [balls, setBalls] = useState(10);
+    // const increment =() =>{
+    //     setBalls(balls+1);
+    // }
+    // const decrement =() =>{
+    //     setBalls(balls-1);
+    // }
+    const [balls, dispatch] = useReducer(reducer, 10);
+  return (
+    <>
+        <h1>Balls</h1>
+        <h2>No. of Balls {balls}</h2>
+        <button onClick={dispatch({type:"increment"})}>+</button>
+        <button onClick={dispatch({type:"decrement"})}>-</button>
+    </>
+  )
+}
+
+export default Ball
